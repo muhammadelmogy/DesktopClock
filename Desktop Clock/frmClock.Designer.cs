5e8c34ca33c0b881db7b572e5852d5a0aaad43f3
Muhammad Elmogy <melmougy@ksu.edu.sa>
2015-03-18 10:41:20 +0300

    Version 1.0 finished
    Signed-off-by: Muhammad Elmogy <melmougy@ksu.edu.sa>

---
+++ Desktop Clock/frmClock.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Desktop_Clock
+{
+    partial class frmClock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblClockDisplay = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblOrangeTheme = new System.Windows.Forms.Label();
+            this.lblBlueTheme = new System.Windows.Forms.Label();
+            this.lblClose = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblClockDisplay
+            // 
+            this.lblClockDisplay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(160)))), ((int)(((byte)(90)))));
+            this.lblClockDisplay.Font = new System.Drawing.Font("Lucida Sans Typewriter", 48F, System.Drawing.FontStyle.Bold);
+            this.lblClockDisplay.Location = new System.Drawing.Point(12, 9);
+            this.lblClockDisplay.Name = "lblClockDisplay";
+            this.lblClockDisplay.Size = new System.Drawing.Size(225, 98);
+            this.lblClockDisplay.TabIndex = 0;
+            this.lblClockDisplay.Text = "label1";
+            this.lblClockDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblClockDisplay.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblClockDisplay_MouseDown);
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Sitka Subheading", 16F);
+            this.lblDate.Location = new System.Drawing.Point(6, 110);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(84, 32);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "lblDate";
+            this.lblDate.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblDate_MouseDown);
+            // 
+            // lblOrangeTheme
+            // 
+            this.lblOrangeTheme.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOrangeTheme.Location = new System.Drawing.Point(222, 122);
+            this.lblOrangeTheme.Name = "lblOrangeTheme";
+            this.lblOrangeTheme.Size = new System.Drawing.Size(15, 15);
+            this.lblOrangeTheme.TabIndex = 3;
+            this.lblOrangeTheme.Click += new System.EventHandler(this.lblOrangeTheme_Click);
+            // 
+            // lblBlueTheme
+            // 
+            this.lblBlueTheme.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBlueTheme.Location = new System.Drawing.Point(201, 122);
+            this.lblBlueTheme.Name = "lblBlueTheme";
+            this.lblBlueTheme.Size = new System.Drawing.Size(15, 15);
+            this.lblBlueTheme.TabIndex = 4;
+            this.lblBlueTheme.Click += new System.EventHandler(this.lblBlueTheme_Click);
+            // 
+            // lblClose
+            // 
+            this.lblClose.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.lblClose.Location = new System.Drawing.Point(222, 9);
+            this.lblClose.Name = "lblClose";
+            this.lblClose.Size = new System.Drawing.Size(15, 15);
+            this.lblClose.TabIndex = 5;
+            this.lblClose.Text = "X";
+            this.lblClose.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblClose.Click += new System.EventHandler(this.lblClose_Click);
+            this.lblClose.MouseEnter += new System.EventHandler(this.lblClose_MouseEnter);
+            this.lblClose.MouseLeave += new System.EventHandler(this.lblClose_MouseLeave);
+            // 
+            // frmClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(108)))), ((int)(((byte)(9)))));
+            this.ClientSize = new System.Drawing.Size(249, 151);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblClose);
+            this.Controls.Add(this.lblBlueTheme);
+            this.Controls.Add(this.lblOrangeTheme);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblClockDisplay);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmClock";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmClock_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblClockDisplay;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblOrangeTheme;
+        private System.Windows.Forms.Label lblBlueTheme;
+        private System.Windows.Forms.Label lblClose;
+    }
+}
+
---
+++ Desktop Clock/frmClock.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desktop_Clock
+{
+    public partial class frmClock : Form
+    {
+        private delegate void RunSetIime(DateTime d);
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HT_CAPTION = 0x2;
+        public frmClock()
+        {
+            InitializeComponent();
+            this.InitComponent();
+
+            this.beginClock();
+        }
+
+        private void beginClock()
+        {
+            Task task = Task.Run(() =>
+            {
+                for (; ;)
+                {
+                    var time = DateTime.Now;
+
+                    this.lblClockDisplay.Invoke(new RunSetIime(SetTime), time);
+
+                    Thread.Sleep((60 - time.Second) * 1000);
+                }
+            });
+        }
+
+        private void SetTime(DateTime d)
+        {
+            var hour = (d.Hour < 10) ? "0" + d.Hour.ToString() : d.Hour.ToString();
+            var min = (d.Minute < 10) ? "0" + d.Minute.ToString() : d.Minute.ToString();
+            var time = string.Format("{0}:{1}", hour, min);
+
+            this.lblClockDisplay.Text = time;
+            this.lblDate.Text = d.Date.ToLongDateString();
+        }
+
+        private void InitComponent()
+        {
+            this.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, this.Width, this.Height, 20, 20));
+            this.lblOrangeTheme.BackColor = Color.FromArgb(255, 108, 9);
+            this.lblBlueTheme.BackColor = Color.FromArgb(24, 116, 205);
+            this.lblClose.Font = this.Font;
+        }
+
+        private void frmClock_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void lblClockDisplay_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.frmClock_MouseDown(this, e);
+        }
+
+        private void lblDate_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.frmClock_MouseDown(this, e);
+        }
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect, // x-coordinate of upper-left corner
+            int nTopRect, // y-coordinate of upper-left corner
+            int nRightRect, // x-coordinate of lower-right corner
+            int nBottomRect, // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+         );
+
+        private void lblOrangeTheme_Click(object sender, EventArgs e)
+        {
+            this.lblClockDisplay.BackColor = System.Drawing.Color.FromArgb(255, 160, 90);
+            this.BackColor = System.Drawing.Color.FromArgb(255, 108, 9);
+            this.lblClose.BackColor = System.Drawing.Color.FromArgb(255, 108, 9);
+        }
+
+        private void lblBlueTheme_Click(object sender, EventArgs e)
+        {
+            this.lblClockDisplay.BackColor = Color.FromArgb(30, 144, 255);
+            this.BackColor = Color.FromArgb(24, 116, 205);
+            this.lblClose.BackColor = Color.FromArgb(24, 116, 205);
+        }
+
+        private void lblClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lblClose_MouseEnter(object sender, EventArgs e)
+        {
+            this.lblClose.Font = new Font(this.Font, FontStyle.Bold);
+        }
+
+        private void lblClose_MouseLeave(object sender, EventArgs e)
+        {
+            this.lblClose.Font = new Font(this.Font, FontStyle.Regular);
+        }
+    }
+}
